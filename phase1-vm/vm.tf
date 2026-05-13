d3173135a1733cb297274b8111807bf967b9dd6e
ben <it_learning_13@protonmail.com>
2026-05-13 18:34:30 +0000

    feat: add phase1

---
+++ phase1-vm/vm.tf
@@ -0,0 +1,128 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  resource_provider_registrations = "none"
+  features {}
+  subscription_id = "subscription_id"
+}
+
+# Resource Group
+resource "azurerm_resource_group" "main" {
+  name     = "rg-terraform-demo"
+  location = "westeurope"
+}
+
+resource "azurerm_resource_group" "demo_state" {
+  name     = "rg-state-demo"
+  location = "westeurope"
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "main" {
+  name                = "vnet-demo"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+# Subnet
+resource "azurerm_subnet" "main" {
+  name                 = "subnet-demo"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Public IP
+resource "azurerm_public_ip" "main" {
+  name                 = "pip-demo"
+  location             = azurerm_resource_group.main.location
+  resource_group_name  = azurerm_resource_group.main.name
+  allocation_method    = "Static"
+}
+
+# Network Security Group (Firewall Rules)
+resource "azurerm_network_security_group" "main" {
+  name                = "nsg-demo"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Network Interface
+resource "azurerm_network_interface" "main" {
+  name                = "nic-demo"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.main.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
+  }
+}
+
+# Connect NSG to NIC
+resource "azurerm_network_interface_security_group_association" "main" {
+  network_interface_id      = azurerm_network_interface.main.id
+  network_security_group_id = azurerm_network_security_group.main.id
+}
+
+# Virtual Machine
+resource "azurerm_linux_virtual_machine" "main" {
+  name                = "vm-demo"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  size                = "Standard_D2s_v3"
+  admin_username      = "azureuser"
+
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/azuredemo.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
+    version   = "latest"
+  }
+}
+
+# Output the public IP so we can SSH
+output "public_ip" {
+  value = azurerm_public_ip.main.ip_address
+}
+
+output "ssh_command" {
+  value = "ssh -i ~/.ssh/azuredemo azureuser@${azurerm_public_ip.main.ip_address}"
+}
